befe07a5832bade8208690dd467c44030ace2372
Aswin6342 <aswinedakulathil@gmail.com>
2026-01-09 11:12:25 +0530

    frontend push

--- autocare-frontend/src/API/api.js
+++ autocare-frontend/src/API/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000";
-
+//const API_URL = "http://localhost:5000";
+const API_URL = "https://autocare-full.onrender.com"
 /* ================= AXIOS INSTANCE ================= */
 const api = axios.create({
   baseURL: API_URL,
