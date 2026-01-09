90ac5bbb66bba92996417c80d36e673865e6d73f
Aswin6342 <aswinedakulathil@gmail.com>
2026-01-09 11:33:38 +0530

    frontend push

--- backend/index.js
+++ backend/index.js
@@ -16,12 +16,14 @@ const port = 5000;
 ======================= */
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://autocare-full.vercel.app",   // 👈 your frontend URL
+    ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 
 /* =======================
    BODY PARSERS
